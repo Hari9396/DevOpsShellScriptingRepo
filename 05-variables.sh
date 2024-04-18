bc26b6d0b204785d75ee1a7b19ddee701a22abfe
Hari <reddy9396@gmail.com>
2024-04-18 21:34:31 +0530

    variable scripts

---
+++ 05-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1="Ramesh"
+Person2="suresh"
+
+echo "$person1:Hello $person2, How are you?"
+echo "$person2: Hello $person1, i am fine how about you"
+echo "$person1:I am good, whats up?"
+echo "$person2:Nothing"
